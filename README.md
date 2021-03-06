e63f3a0939ee567cb4caf0669173416d012b5f66
Utri Gergely <utrig@yahoo.com>
2021-03-06 18:29:32 +0100

    added some comments

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Siargao and the intuitive menu
 
-A landing page of a holiday destination using mainly SVG images and a smart hamburger menu that can read your thoughts as you change the size and shape of the window. It even indicates emotions! Try it, it does exactly what you intended it to do.
+A landing page of a holiday destination using mainly SVG images and a smart hamburger menu that can read your thoughts as you change the size and shape of the window. It even indicates emotions! Try it, it does exactly what you intended it to do. I used an old-school JS code for that, as it was my first attempt to predict user behavior as a beginner coder.
--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+//I used inline style to see the mechanism
 var visible = false;
 const m = document.getElementById("menu");
 const qw = window.matchMedia("(max-width: 920px)");
@@ -7,11 +8,12 @@ qw.addListener(shrink);
 qh.addListener(shrink);
 
 function popup() {
+  //Toggles normal appearance as you click on the menu icon.
+  //The menu disappears automatically if it overlaps with the text, as the user probably wants to read the text.
   if (visible) {
     m.style.opacity = "0";
     m.style.height = "0";
     visible = false;
-  
   } else {
     m.innerHTML = `<a href="#top" class="menuitem">Top</a>
     <a href="#paradise" class="menuitem">First impressions</a>
@@ -20,19 +22,22 @@ function popup() {
     m.style.opacity = "1";
     visible = true;
     m.style.fontSize = "1.1em";
-    m.style.flexDirection = "column"
+    m.style.flexDirection = "column";
     m.style.width = "fit-content";
     m.style.height = "256px";
     m.style.backgroundColor = "#f8ffb8";
-    
+
+    //However, if the user opens the menu again in a smaller screen, it does cover the text, as the user propbably wants to read the menu.
+    //Vertical (re-)appearance in a small portrait view.
     if (qw.matches) {
       m.style.fontSize = "1.3em";
-      m.style.flexDirection = "column"
+      m.style.flexDirection = "column";
       m.style.height = "272px";
       m.style.width = "425px";
       m.style.maxHeight = "fit-content";
       m.style.backgroundColor = "#d42000";
-    } 
+    }
+    //Horizontal (re-)appearance in a small landscape view
     if (qh.matches) {
       m.style.fontSize = "1em";
       m.style.flexDirection = "row";
@@ -46,8 +51,7 @@ function popup() {
 
 function shrink(qw, qh) {
   if (qw.matches || qh.matches) {
-    visible = true; popup()
+    visible = true;
+    popup();
   }
 }
-
-
